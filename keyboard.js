503c6c18541d2f825b929d4f75af8c40f10c1ab7
Peter Leinonen <peter@leinonen.se>
2015-05-11 22:25:47 +0200

    indentation

--- keyboard.js
+++ keyboard.js
@@ -11,76 +11,76 @@ var scales = require('./scales');
 
 
 function mapKey(inkey) {
-	return notes[(inkey % 12)];
+  return notes[(inkey % 12)];
 }
 
 function unMapNote(note) {
-	return notes.indexOf(note);
+  return notes.indexOf(note);
 }
 
 function printSequence() {
-	var seq = sequenceToPlay.map(function (a) {
-		return mapKey(a);
-	}).join(' ');
-	console.log(seq);
+  var seq = sequenceToPlay.map(function (a) {
+    return mapKey(a);
+  }).join(' ');
+  console.log(seq);
 }
 
 
 function generateSequence() {
-	currentScale = Math.floor(Math.random() * scales.length);
-	sequenceToPlay = scales[currentScale].notes.map(unMapNote);
-	console.log('Please play the %s', scales[currentScale].name);
+  currentScale = Math.floor(Math.random() * scales.length);
+  sequenceToPlay = scales[currentScale].notes.map(unMapNote);
+  console.log('Please play the %s', scales[currentScale].name);
 }
 
 function validNote(key) {
-	return sequenceToPlay[0] === (key % 12);
+  return sequenceToPlay[0] === (key % 12);
 }
 
 function noteSuccess() {
-	console.log('CORRECT!');
-	sequenceToPlay.splice(0, 1);
+  console.log('CORRECT!');
+  sequenceToPlay.splice(0, 1);
 }
 
 function noteFail() {
-	console.log('WRONG!');
+  console.log('WRONG!');
 }
 
 function sequenceComplete() {
-	return sequenceToPlay.length === 0;
+  return sequenceToPlay.length === 0;
 }
 
 function play() {
-	generateSequence();
-	printSequence();
+  generateSequence();
+  printSequence();
 }
 
 function win() {
-	console.log('You are good!');
+  console.log('You are good!');
 }
 
 function processMessage(deltaTime, message) {
-	var key = message[1];
-	var dir = message[0];
+  var key = message[1];
+  var dir = message[0];
 
-	// 144 = down press, 128 = release
-	if (dir === 144) {
-		currentKey = key;
-	} else if (dir === 128 && currentKey === key) {
+  // 144 = down press, 128 = release
+  if (dir === 144) {
+    currentKey = key;
+  } else if (dir === 128 && currentKey === key) {
 
-		if (validNote(key)) {
-			noteSuccess();
-			if (sequenceComplete()) {
-				win();
-				play();
-			} else {
-				printSequence();
-			}
-		}
+    if (validNote(key)) {
+      noteSuccess();
+      if (sequenceComplete()) {
+        win();
+        play();
+      } else {
+        printSequence();
+      }
+    }
 
-	} else {
-		noteFail();
-		printSequence();
-	}
+  } else {
+    noteFail();
+    printSequence();
+  }
 
 }
 
